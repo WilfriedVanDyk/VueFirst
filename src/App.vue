ab1b2da4ae3025d63df7095f1933ba97dc1d13c6
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-06-01 10:03:52 +0200

    titel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Vue.js App"/>
+    <HelloWorld msg="Welcome togit a Vue.js App"/>
   </div>
 </template>
 
