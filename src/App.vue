d603675929db57633f750c631a62503362c043b0
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-06-07 13:19:32 +0200

    boodschap voor Corneel en Bram

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Vue.js App"/>
+    <HelloWorld msg="Dag Corneel en Bram van Publiq"/>
   </div>
 </template>
 
