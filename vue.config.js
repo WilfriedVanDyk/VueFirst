463c6ebf514be2cc81149392e49a8c318d3a9802
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-06-14 13:34:47 +0200

    paging add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-module.exports = {
-    publicPath: "/serve-vue-app",
-    outputDir: "docs"
+
+  module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/VueFirst/'
+      : '/'
   };
